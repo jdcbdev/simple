75dcca1c19cbcdd2b0fe03f77b048e312a90764f
Jaydee Ballaho <jaydee.ballaho@wmsu.edu.ph>
2022-07-13 03:30:52 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,26 +5,17 @@
 error_reporting(-1);
 ini_set('display_errors', 'On');
 
-$dbname = 'test';
-$dbuser = 'sample';
-$dbpass = 'sample';
-$dbhost = 'localhost';
+ $host = 'localhost';
+ $user = 'sample';
+ $password = 'sample';
+ $database = 'test';
+ $mysqli = new MySQLi($host, $user, $password, $database);
 
-$link = mysqli_connect($dbhost, $dbuser, $dbpass) or die("Unable to Connect to '$dbhost'");
-mysqli_select_db($link, $dbname) or die("Could not open the db '$dbname'");
+ if($mysqli->connect_error) {
+  echo 'Connection Failed!
+  Error #' . $mysqli->connect_errno
+  . ': ' . $mysqli->connect_error;
+  exit(0);
+ }
 
-$test_query = "SHOW TABLES FROM $dbname";
-$result = mysqli_query($link, $test_query);
-
-$tblCnt = 0;
-while($tbl = mysqli_fetch_array($result)) {
-  $tblCnt++;
-  #echo $tbl[0]."<br />\n";
-}
-
-if (!$tblCnt) {
-  echo "There are no tables<br />\n";
-} else {
-  echo "There are $tblCnt tables<br />\n";
-} 
 ?>
